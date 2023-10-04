0aa70c4620c3b5297adf3d9d35d9b618c9e5fc9d
tatsumiakahori <seadragon0828@gmail.com>
2023-10-04 22:25:37 +0900

    一旦完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { fetchImages } from "./api";
+
 function Header() {
   return (
     <header className="hero is-dark is-bold">
@@ -22,8 +25,14 @@ function Image(props) {
   );
 }
 
+function Loading() {
+  return <p>Loading...</p>;
+}
+
 function Gallery(props) {
-  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+  if (props.urls == null) {
+    return <Loading />;
+  }
   return (
     <div className="columns is-vcentered is-multiline">
       {props.urls.map((url) => {
@@ -37,23 +46,54 @@ function Gallery(props) {
   );
 }
 
+function Form(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { breed } = event.target.elements;
+    props.onFormSubmit(breed.value)
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select name="breed" defaultValue="shiba">
+                <option value="shiba">Shiba</option>
+                <option value="akita">Akita</option>
+              </select>
+            </div>
+          </div>
+          <div className="controll">
+            <button type="submit" className="button is-dark">
+              Reload              
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function Main() {
-  const urls = [
-    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
-  ];
+  const [urls, setUrls] = useState(null);
+  useEffect(() => {
+    fetchImages("shiba").then((urls) => {
+      setUrls(urls);
+    });
+  }, []);
+  const reloadImages = (breed) => {
+    fetchImages(breed).then((urls) => {
+      setUrls(urls);
+    });
+  }
   return (
     <main>
+      <section className="section">
+        <div className="container">
+          <Form onFormSubmit={reloadImages} />
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <Gallery urls={urls} />
